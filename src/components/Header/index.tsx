04417d196d9377d49cfc626ba0eafb698e2fec44
Manprit <mnprt312@gmail.com>
2025-10-05 13:10:04 +0530

    theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Search, ShoppingCart, User, ChevronDown, Menu, X } from "lucide-react";
 import Image from "next/image";
 
-export default function Header({cart}) {
+export default function Header({cart,menuOpen,setMenuOpen}: {cart: any[], menuOpen: boolean, setMenuOpen: (open: boolean) => void}) {
   const [hoveredMenu, setHoveredMenu] = useState(null);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
