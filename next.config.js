d794c15da203a70637e2f169af03708009489004
Manprit <mnprt312@gmail.com>
2025-11-02 23:10:54 +0530

    3d conmfig

--- next.config.js
+++ next.config.js
@@ -16,24 +16,7 @@ const nextConfig = {
     });
     return config;
   },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin'
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp'
-          },
-           { key: "Access-Control-Allow-Origin", value: "*" },
-        ]
-      }
-    ];
-  }
+
 };
 
 module.exports = nextConfig;
