299b0a7603bf8d7a9ef3d664ae963962a1e36bdd
Manprit <mnprt312@gmail.com>
2025-11-02 23:05:03 +0530

    3d

--- src/components/Home/ProductCard.tsx
+++ src/components/Home/ProductCard.tsx
@@ -44,7 +44,7 @@ console.log(product.model3D, is3D);
                  hover:scale-[1.02] transition-all duration-300"
     >
       {/* === 3D Viewer or Fallback === */}
-      {/* {is3D ? ( */}
+      {is3D ? (
         <model-viewer
           src={product.model3D}
           alt={product.name}
@@ -57,7 +57,7 @@ console.log(product.model3D, is3D);
             borderBottom: "1px solid #e7f6ed",
           }}
         ></model-viewer>
-      {/* ) : product.images?.length ? (
+       ) : product.images?.length ? (
         <img
           src={product.images[0]}
           alt={product.name}
@@ -67,7 +67,7 @@ console.log(product.model3D, is3D);
         <div className="aspect-[4/3] flex items-center justify-center text-gray-400 text-sm italic bg-[#f5fff9]">
           No Image
         </div>
-      )} */}
+      )} 
 
       {/* === Product Info === */}
       <div className="p-4 sm:p-5">
